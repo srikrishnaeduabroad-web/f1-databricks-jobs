c5f14eb466cb822a000c59c9e06481275f397217
srikrishnaeduabroad-web <srikrishna.eduabroad@gmail.com>
2026-04-08 11:51:01 -0500

    Statements to clean the bronze layer

---
+++ call_all_sp/clean_bronze_layer.sql
@@ -0,0 +1,26 @@
+TRUNCATE TABLE f1_data_lakehouse.bronze.circuits ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.constructors ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.constructors_results ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.constructors_standings ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.drivers ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.driver_standings ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.lap_times ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.pit_stops ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.qualifying ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.races  ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.results  ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.seasons  ;
+
+TRUNCATE TABLE f1_data_lakehouse.bronze.status  ;
+
